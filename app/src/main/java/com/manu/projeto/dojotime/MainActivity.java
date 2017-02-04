c6c8f20a4c0afd6fad932d13d1ec21a527d69ed9
ManuJS <emanuelle.menali@hotmail.com>
2017-02-04 02:48:34 -0200

    coloquei acoes toast  nos botoes

--- app/src/main/java/com/manu/projeto/dojotime/MainActivity.java
+++ app/src/main/java/com/manu/projeto/dojotime/MainActivity.java
@@ -3,12 +3,15 @@ package com.manu.projeto.dojotime;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.manu.projeto.dojotime.databinding.LayoutTimerPrincipalBinding;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
 
     LayoutTimerPrincipalBinding binding;
+    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +19,22 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
 
         binding = DataBindingUtil.setContentView(this, R.layout.layout_timer_principal);
 
-        binding.imageButtonPause.
+        binding.editTime.setText("4:29");
+//        binding.imageButtonPlay
+
+        toast = Toast.makeText(this,"Play", Toast.LENGTH_SHORT );
+
+        binding.imageButtonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toast.show();
+            }
+        });
 
         contatempo();
     }
 
     public void contatempo(){}
+
+
 }
