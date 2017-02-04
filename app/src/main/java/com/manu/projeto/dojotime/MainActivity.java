5bf2b9eec813104dec63cf282e60f17389448d3b
ManuJS <emanuelle.menali@hotmail.com>
2017-02-04 02:28:16 -0200

    chamando os componentes de tela

--- app/src/main/java/com/manu/projeto/dojotime/MainActivity.java
+++ app/src/main/java/com/manu/projeto/dojotime/MainActivity.java
@@ -2,12 +2,30 @@ package com.manu.projeto.dojotime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements MainContract.View {
+
+    TextView txtTempo;
+    ImageButton play;
+    ImageButton pause;
+    ImageButton stop;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_timer_principal);
+
+        txtTempo = (TextView) findViewById(R.id.editTime);
+        play = (ImageButton) findViewById(R.id.imageButtonPlay);
+        pause = (ImageButton) findViewById(R.id.imageButtonPause);
+        stop = (ImageButton) findViewById(R.id.imageButtonStop);
+
+        contatempo();
     }
+
+    public void contatempo(){}
 }
--- app/src/main/java/com/manu/projeto/dojotime/Timer.java
+++
@@ -1,8 +0,0 @@
-package com.manu.projeto.dojotime;
-
-/**
- * Created by emanu on 04/02/2017.
- */
-
-public class Timer {
-}
